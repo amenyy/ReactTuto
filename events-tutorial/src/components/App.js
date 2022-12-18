377be55ade259dd7338e7b8b6eb0a663b0fcfabc
MeowDevelopment <ameny.ferchichi99@gmail.com>
2022-12-18 10:27:54 +0100

    last commit

---
+++ events-tutorial/src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import FileNamer from 'C:/Users/DELL/Desktop/HomeWork React/events-tutorial/src/components/FileNamer/FileNamer.js';
+
+function App() {
+    return <FileNamer />
+}
+
+export default App;
